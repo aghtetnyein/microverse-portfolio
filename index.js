9cb223c7973f917f4c003bddabccbbcb733681bd
Aung Htet Nyein <aunghtetnyein1999@gmail.com>
2022-09-06 00:08:53 +0630

    Fixed empty space bug at index.js

--- index.js
+++ index.js
@@ -2,24 +2,24 @@ const navModal = document.getElementById("navigation-modal");
 const menu = document.getElementById("menu");
 const close = document.getElementById("close");
 const navLinks = document.querySelectorAll("#navigation-modal a");
-​
+
 menu.addEventListener("click", () => {
   navModal.style.left = 0;
   disableScroll();
 });
-​
+
 close.addEventListener("click", () => {
   navModal.style.left = "100vw";
   enableScroll();
 });
-​
+
 for (let i = 0; i < navLinks.length; i++) {
   navLinks[i].addEventListener("click", () => {
     navModal.style.left = "100vw";
     enableScroll();
   });
 }
-​
+
 // call this to Disable
 function disableScroll() {
   var xPos = window.scrollX;
@@ -28,7 +28,7 @@ function disableScroll() {
     window.scroll(xPos, yPos);
   };
 }
-​
+
 // call this to Enable
 function enableScroll() {
   window.onscroll = "";
